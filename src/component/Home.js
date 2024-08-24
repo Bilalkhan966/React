5ebbf3c2aef6dcbb0bcf7c198ed6c53a61bf45fe
khan je@BilalShinwari <bilal96shinwari@gmail.com>
2024-08-24 11:51:12 +0500

    Add Bootstap via CDN

---
+++ src/component/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './Home.css'
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className='test'>This is a first paragraph from home componet</h1>
+        <button className='btn btn-primary'>Click Here</button>
+      </div>
+    )
+  }
+}
